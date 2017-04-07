df4c9b6db441850d7589546fe898e0e098429ba4
Emily Keen <emily.keen711@gmail.com>
2017-04-07 12:02:04 -0400

    ace of base

--- liri.js
+++ liri.js
@@ -44,115 +44,112 @@ if (command === "my-tweets") {
 } else if (command === "spotify-this-song") {
 
     var song = (process.argv[3])
-    if ((process.argv[3]) === undefined) {
-        song = ("The Sign");
-        console.log('false');
-    }
-    request("https://api.spotify.com/v1/search?q=" + song + "&type=track&limit=1", function(error, response, body) {
-
-        // If there were no errors and the response code was 200 (i.e. the request was successful)...
-        if (!error && response.statusCode === 200) {
-
-            //Artist(s)
-
-            console.log("The artist is: " + JSON.parse(body).tracks.items[0].artists[0].name);
-            // The song's name
-            console.log("The song name is: " + JSON.parse(body).tracks.items[0].name);
-            // A preview link of the song from Spotify
-            console.log("A preview link of song: " + JSON.parse(body).tracks.items[0].preview_url);
-            // The album that the song is from
-            console.log("The album is: " + JSON.parse(body).tracks.items[0].album.name);
-
-
-        }
-    });
-
-
-
-
-
-
-
-
-    //Artist(s)
-    // The song's name
-    // A preview link of the song from Spotify
-    // The album that the song is from
-
 
     // if no song is provided then your program will default to
     // "The Sign" by Ace of Base
-
-
-
-} else if (command === "movie-this") {
-    var movie = (process.argv[3])
-
     if ((process.argv[3]) === undefined) {
-        //if no user input for song, assign a song
-        movie = ("Mr.Nobody");
         console.log('false');
-        console.log("If you haven't watched 'Mr. Nobody,'then you should: http://www.imdb.com/title/tt0485947/")
-        console.log("It's on Netflix!");
-    }
+        request("https://api.spotify.com/v1/artists/5ksRONqssB7BR161NTtJAm/top-tracks?country=US", function(error, response, body) {
+             //Artist(s)
+                console.log("Artist: "+ JSON.parse(body).tracks[1].artists[0].name);
+                 // The song's name
+                  console.log("Song: "+ JSON.parse(body).tracks[1].name);
+                 // A preview link of the song from Spotify
+                 console.log("Preview Link: "+ JSON.parse(body).tracks[1].preview_url);
+                 // The album that the song is from
+                 console.log("album: "+ JSON.parse(body).tracks[1].album.name);
+            })
+        }
+            request("https://api.spotify.com/v1/search?q=" + song + "&type=track&limit=1", function(error, response, body) {
 
-    // We then run the request module on a URL with a JSON
-    request("http://www.omdbapi.com/?t=" + movie + "&y=&plot=short&r=json", function(error, response, body) {
+                // If there were no errors and the response code was 200 (i.e. the request was successful)...
+                if (!error && response.statusCode === 200 && (process.argv[3]) !== undefined) {
 
-        // If there were no errors and the response code was 200 (i.e. the request was successful)...
-        if (!error && response.statusCode === 200) {
+                    //Artist(s)
 
-            // * Title of the movie.
-            console.log("The movie's title is: " + JSON.parse(body).Title);
-            // * Year the movie came out.
-            console.log("The movie came out in : " + JSON.parse(body).Released);
-            // * IMDB Rating of the movie.
-            console.log("The movie's IMDB rating is: " + JSON.parse(body).imdbRating);
-            // * Country where the movie was produced.
-            console.log("The movie was produced in: " + JSON.parse(body).Country);
-            // * Language of the movie.
-            console.log("The movie's language is: " + JSON.parse(body).Language);
-            // * Plot of the movie.
-            console.log("The movie's plot is: " + JSON.parse(body).Plot);
-            // * Actors in the movie.
-            console.log("The movie's actors are: " + JSON.parse(body).Actors);
-            // * Rotten Tomatoes Rating.
-            console.log("The movie's Rotten Tomatoes Rating is: " + JSON.parse(body).Ratings[1].Value)
+                    console.log("The artist is: " + JSON.parse(body).tracks.items[0].artists[0].name);
+                    // The song's name
+                    console.log("The song name is: " + JSON.parse(body).tracks.items[0].name);
+                    // A preview link of the song from Spotify
+                    console.log("A preview link of song: " + JSON.parse(body).tracks.items[0].preview_url);
+                    // The album that the song is from
+                    console.log("The album is: " + JSON.parse(body).tracks.items[0].album.name);
+
+
+                }
+            });
+
+
 
         }
-    });
+        else if (command === "movie-this") {
+            var movie = (process.argv[3])
 
-} else if (command === "do-what-it-says") {
-
-    fs.readFile("random.txt", "utf8", function(error, data) {
-        //split data from random.txt using comma and store in an Array for use as parameters for function call
-        var dataArr = data.split(",");
-        console.log(dataArr);
-        console.log(dataArr[0], dataArr[1]);
-        song = (dataArr[1]);
-        console.log(song);
-
-        request("https://api.spotify.com/v1/search?q=" + song + "&type=track&limit=1", function(error, response, body) {
-
-            // If there were no errors and the response code was 200 (i.e. the request was successful)...
-            if (!error && response.statusCode === 200) {
-
-                //Artist(s)
-
-                console.log("The artist is: " + JSON.parse(body).tracks.items[0].artists[0].name);
-                // The song's name
-                console.log("The song name is: " + JSON.parse(body).tracks.items[0].name);
-                // A preview link of the song from Spotify
-                console.log("A preview link of song: " + JSON.parse(body).tracks.items[0].preview_url);
-                // The album that the song is from
-                console.log("The album is: " + JSON.parse(body).tracks.items[0].album.name);
-
-
+            if ((process.argv[3]) === undefined) {
+                //if no user input for song, assign a song
+                movie = ("Mr.Nobody");
+                console.log('false');
+                console.log("If you haven't watched 'Mr. Nobody,'then you should: http://www.imdb.com/title/tt0485947/")
+                console.log("It's on Netflix!");
             }
-        });
+
+            // We then run the request module on a URL with a JSON
+            request("http://www.omdbapi.com/?t=" + movie + "&y=&plot=short&r=json", function(error, response, body) {
+
+                // If there were no errors and the response code was 200 (i.e. the request was successful)...
+                if (!error && response.statusCode === 200) {
+
+                    // * Title of the movie.
+                    console.log("The movie's title is: " + JSON.parse(body).Title);
+                    // * Year the movie came out.
+                    console.log("The movie came out in : " + JSON.parse(body).Released);
+                    // * IMDB Rating of the movie.
+                    console.log("The movie's IMDB rating is: " + JSON.parse(body).imdbRating);
+                    // * Country where the movie was produced.
+                    console.log("The movie was produced in: " + JSON.parse(body).Country);
+                    // * Language of the movie.
+                    console.log("The movie's language is: " + JSON.parse(body).Language);
+                    // * Plot of the movie.
+                    console.log("The movie's plot is: " + JSON.parse(body).Plot);
+                    // * Actors in the movie.
+                    console.log("The movie's actors are: " + JSON.parse(body).Actors);
+                    // * Rotten Tomatoes Rating.
+                    console.log("The movie's Rotten Tomatoes Rating is: " + JSON.parse(body).Ratings[1].Value)
+
+                }
+            });
+
+        } else if (command === "do-what-it-says") {
+
+            fs.readFile("random.txt", "utf8", function(error, data) {
+                //split data from random.txt using comma and store in an Array for use as parameters for function call
+                var dataArr = data.split(",");
+                console.log(dataArr);
+                console.log(dataArr[0], dataArr[1]);
+                song = (dataArr[1]);
+                console.log(song);
+
+                request("https://api.spotify.com/v1/search?q=" + song + "&type=track&limit=1", function(error, response, body) {
+
+                    // If there were no errors and the response code was 200 (i.e. the request was successful)...
+                    if (!error && response.statusCode === 200) {
+
+                        //Artist(s)
+
+                        console.log("The artist is: " + JSON.parse(body).tracks.items[0].artists[0].name);
+                        // The song's name
+                        console.log("The song name is: " + JSON.parse(body).tracks.items[0].name);
+                        // A preview link of the song from Spotify
+                        console.log("A preview link of song: " + JSON.parse(body).tracks.items[0].preview_url);
+                        // The album that the song is from
+                        console.log("The album is: " + JSON.parse(body).tracks.items[0].album.name);
+
+
+                    }
+                });
 
 
 
 
-    });
-}
+            });
+        }
